6ac35b22a797423be4c1312953e7676463096757
Colton Nielsen <cltnnielsen@gmail.com>
2020-07-17 22:19:16 -0600

    added more functionailty to Build A Quiz component *known bug for this.state.quizFourm.questions.map on line 104*

--- src/components/BuildAQuiz/BuildAQuiz.js
+++ src/components/BuildAQuiz/BuildAQuiz.js
@@ -3,44 +3,115 @@ import React, { Component } from 'react'
 //Components
 import BuildAQuizButton from './BuildAQuizButton/BuildAQuizButton'
 import QuizForum from './QuizForum/QuizForum'
+import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
 
 export default class BuildAQuiz extends Component {
 
     state = {
         quizForum: {
             name: '',
-            question: '',
-            answer: '',
-            fakeAnswer1: '',
-            fakeAnswer2: '',
-            fakeAnswer3: '',
+            questions: [
+                    {
+                        question: '',
+                        answer: '',
+                        fakeAnswer1: '',
+                        fakeAnswer2: '',
+                        fakeAnswer3: '',
+                    }, 
+                ],
         },
-        isQuizForumVisible: false
+        isQuizForumVisible: true,
     }
 
     handleQuizVisible = () => {
         this.setState( prevState => ({isQuizForumVisible: !prevState.isQuizForumVisible}))
     }
 
-    handleChange = (e) => {
-        this.setState({quizForum : {
-            ...this.state.quizForum,
+    handleChange = (e, key) => {
+        //Make shallow copy of the questions array
+        let currArr = {
+            ...this.state.quizForum.questions,        
+        }
+
+        //Copy to a different variable the specific quiz that you would like to edit
+        let currQuiz = {
+            ...currArr[key],
             [e.target.name] : e.target.value
-        }})
+        }
+        
+        //Save the edited quizQuestion to your copy of the array
+        currArr[key] = currQuiz;
+
+        this.setState({
+            quizForum: {
+                questions: currArr
+            }
+        })
+        console.log(this.state)
     }
 
     handleSubmit = (e) => {
         e.preventDefault()
         console.log(this.state.quizForum)
+
+        // this.setState({
+        //     quizForum: {
+        //         name: '',
+        //         questions: {
+        //             question1:{
+        //                 question: '',
+        //                 answer: '',
+        //                 fakeAnswer1: '',
+        //                 fakeAnswer2: '',
+        //                 fakeAnswer3: '',
+        //             }, 
+        //         },
+        //     },
+        //     isQuizForumVisible: false
+        // })
+    }
+
+    handleQuizNameChange = (e, key) => {
+        this.setState({quizForum:{
+            ...this.state.quizForum,
+            name: e.target.value
+        }})
+        console.log(this.state)
+    }
+
+    handleAddQuestion = () =>{
+        let questionTemplate = {
+            question: '',
+            answer: '',
+            fakeAnswer1: '',
+            fakeAnswer2: '',
+            fakeAnswer3: '',
+        }
+        this.setState({
+            quizForum: {
+                questions: [
+                    ...this.state.quizForum.questions,
+                    questionTemplate
+                ]
+            }
+        })
+        setTimeout(() => console.log(this.state.key), 500)
     }
 
     render() {
         let quizForum = null;
         if(this.state.isQuizForumVisible){
-            quizForum = (
-                <QuizForum
-                submit = {this.handleSubmit}
-                handleChange={this.handleChange}/>
+            quizForum = this.state.quizForum.questions.map( (question , index) => (
+                <Auxiliary>
+                    <h1>Create A New Quiz:</h1>
+                    <QuizForum
+                    key={index}
+                    handleQuizNameChange={this.handleQuizNameChange}
+                    submit = {this.handleSubmit}
+                    handleChange={this.handleChange}
+                    addQuestion = {this.handleAddQuestion}/>
+                </Auxiliary>
+                )
             )
         }
 
--- src/components/BuildAQuiz/QuizForum/QuizForum.js
+++ src/components/BuildAQuiz/QuizForum/QuizForum.js
@@ -4,58 +4,64 @@ import classes from './QuizForum.module.css'
 export default function QuizForum(props) {
     return (
         <form onSubmit={props.submit}className={classes.Form}>
-            <h1>Create A New Quiz:</h1>
-            <div className={classes.InputGroup}>
+            <div className={[classes.InputGroup, classes.InputStyle1].join(' ')}>
 
                 <input
+                required
                 placeholder='Quiz Name'
                 name='name' 
                 type='text'
-                onChange={props.handleChange}
+                onChange={props.handleQuizNameChange}
                 />
-                
+            </div>
 
+            <div className={classes.InputGroup}>
                 <input
+                required
                 placeholder='Question'
                 name='question'
                 type='text'
-                onChange={props.handleChange}
+                onChange={(e) => props.handleChange(e, props.key)}
                 />
 
-            </div>
-
-            <div className={classes.InputGroup}>
                 <input
+                required
                 placeholder='Anwser'
                 name='answer'
                 type='text'
-                onChange={props.handleChange}
+                onChange={(e) => props.handleChange(e, props.key)}
                 />
             </div>
 
             <div className={classes.InputGroup}>
                 <input
+                required
                 placeholder='Fake Answer #1'
                 name='fakeAnswer1'
                 type='text'
-                onChange={props.handleChange}
+                onChange={(e) => props.handleChange(e, props.key)}
                 />
 
                 <input
+                required
                 placeholder='Fake Answer #2'
                 name='fakeAnswer2'
                 type='text'
-                onChange={props.handleChange}
+                onChange={(e) => props.handleChange(e, props.key)}
                 />
 
                 <input
+                required
                 placeholder='Fake Answer #3'
                 name='fakeAnswer3'
                 type='text'
-                onChange={props.handleChange}
+                onChange={(e) => props.handleChange(e, props.key)}
                 />
             </div>
-            <button className={[classes.Button, classes.ButtonGreen].join(' ')}> Submit </button>
+            <div className={classes.ButtonContainer}>  
+                <p onClick={props.addQuestion} className={[classes.Button, classes.ButtonYellow].join(' ')}>Add Question</p>
+                <button type='submit' className={[classes.Button, classes.ButtonGreen].join(' ')}> Submit </button>
+            </div>
         </form>
     )
 }
